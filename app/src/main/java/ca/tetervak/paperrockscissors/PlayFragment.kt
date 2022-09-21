a5b72084330a1f50bb9e1f642f053fb860bc0270
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-09-21 10:48:36 -0400

    preselect random choice onStart

--- app/src/main/java/ca/tetervak/paperrockscissors/PlayFragment.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors/PlayFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import ca.tetervak.paperrockscissors.databinding.FragmentPlayBinding
+import ca.tetervak.paperrockscissors.model.Choice
+import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
 
 class PlayFragment : Fragment() {
 
@@ -14,6 +19,8 @@ class PlayFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,10 +28,43 @@ class PlayFragment : Fragment() {
         _binding = FragmentPlayBinding.inflate(inflater, container, false)
 
         binding.playButton.setOnClickListener {
-
+            val userChoice = getUserChoice()
+            if(userChoice != null){
+                mainViewModel.setUserChoice(userChoice)
+                findNavController().navigate(R.id.action_playFragment_to_resultFragment)
+            } else {
+                remindToSelect()
+            }
         }
-
         return binding.root
+    }
+
+    private fun getUserChoice(): Choice? {
+        return when(binding.userChoiceGroup.checkedRadioButtonId){
+            R.id.paper_button -> Choice.PAPER
+            R.id.rock_button -> Choice.ROCK
+            R.id.scissors_button -> Choice.SCISSORS
+            else -> null
+        }
+    }
+
+    private fun remindToSelect() {
+        Snackbar.make(binding.root, getString(R.string.make_your_choice), Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun selectRandomChoice(){
+        with(binding) {
+            when ((1..3).random()) {
+                1 -> paperButton.isChecked = true
+                2 -> rockButton.isChecked = true
+                3 -> scissorsButton.isChecked = true
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        selectRandomChoice()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ca/tetervak/paperrockscissors/ResultFragment.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors/ResultFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import ca.tetervak.paperrockscissors.databinding.FragmentResultBinding
+import ca.tetervak.paperrockscissors.model.Choice
+import ca.tetervak.paperrockscissors.model.GameData
+import ca.tetervak.paperrockscissors.model.GameResult
+
 
 class ResultFragment : Fragment() {
 
     private var _binding: FragmentResultBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,11 +29,37 @@ class ResultFragment : Fragment() {
     ): View {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
 
-        binding.repeatButton.setOnClickListener {
+        mainViewModel.gameData.observe(viewLifecycleOwner) { data ->
+            updateOutputs(data)
+        }
 
+        binding.repeatButton.setOnClickListener {
+            findNavController().popBackStack()
         }
 
         return binding.root
+    }
+
+    private fun updateOutputs(data: GameData) {
+        binding.computerChoice.text = choiceToString(data.playerOneChoice)
+        binding.userChoice.text = choiceToString(data.playerTwoChoice)
+        binding.resultMessage.text = resultToString(data.result)
+    }
+
+    private fun choiceToString(choice: Choice): String {
+        return when (choice) {
+            Choice.PAPER -> getString(R.string.paper)
+            Choice.ROCK -> getString(R.string.rock)
+            Choice.SCISSORS -> getString(R.string.scissors)
+        }
+    }
+
+    private fun resultToString(result: GameResult): String {
+        return when (result) {
+            GameResult.PLAYER_ONE_WINS -> getString(R.string.computer_wins)
+            GameResult.PLAYER_TWO_WINS -> getString(R.string.you_win)
+            GameResult.REPLAY -> getString(R.string.replay)
+        }
     }
 
     override fun onDestroyView() {
