fcdd524d4f2589363a50cd2363231da66948505f
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-09-21 11:31:17 -0400

    added about dialog

--- app/src/main/java/ca/tetervak/paperrockscissors/MainActivity.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors/MainActivity.kt
@@ -41,7 +41,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_about -> {
+                val navController = findNavController(R.id.nav_host_fragment_content_main)
+                navController.navigate(R.id.action_global_aboutFragment)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
